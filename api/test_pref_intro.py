bd1355f4f8ee6f083996bccb01d972f7cfc5d35d
m212 <m212@skytree2023.site>
2023-07-03 16:32:39 +0900

    by okamoto

--- api/test_pref_intro.py
+++ api/test_pref_intro.py
@@ -7,9 +7,13 @@ Test Travel Introduction by ChatGPTAPI
 '''
 client = TestClient(app)
 def test_read_main():
+    #ex) "滋賀"
     response = client.get("/pref/滋賀")
     assert response.status_code == 200
+    
+    #ex) "shiga"
     response = client.get("/pref/shiga")
+    assert response.status_code == 200
     assert response.json()["message"] == "invalid name error"
 
 if __name__=="__main__":
