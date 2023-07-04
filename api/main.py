ea75a2d20a99c7d85f217a717d28425e92f96128
m211 <0853203@mchcgr.com>
2023-07-04 13:59:47 +0900

    111

--- api/main.py
+++ api/main.py
@@ -15,32 +15,6 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-
-class Item(BaseModel):
-    name: str
-    price: float
-    is_offer: bool = None
-
-
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
-
-
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: str = None):
-    return {"item_id": item_id, "q": q}
-
-
-@app.put("/items/{item_id}")
-def update_item(item_id: int, item: Item):
-    return {"item_name": item.name, "item_id": item_id}
-
-
-@app.delete("/items/{item_id}")
-def update_item(item_id: int, item: Item):
-    return {"item_name": item.name, "item_price": item.price}
-
 #Prefectures introduction
 @app.get("/pref")
 def read_pref():
