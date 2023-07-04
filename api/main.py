00a6c03e51d8f23deafce2532c7acdc6ec4b992b
m212 <m212@skytree2023.site>
2023-07-04 13:53:37 +0900

    by okamoto

--- api/main.py
+++ api/main.py
@@ -41,73 +41,16 @@ def update_item(item_id: int, item: Item):
 def update_item(item_id: int, item: Item):
     return {"item_name": item.name, "item_price": item.price}
 
-# Prefectures introduction
-
-
+#Prefectures introduction
 @app.get("/pref")
 def read_pref():
     return {"Input": "pref_name"}
 
-
-# 都道府県名のリスト
-prefectures = [
-    "北海道",
-    "青森県",
-    "岩手県",
-    "宮城県",
-    "秋田県",
-    "山形県",
-    "福島県",
-    "茨城県",
-    "栃木県",
-    "群馬県",
-    "埼玉県",
-    "千葉県",
-    "東京都",
-    "神奈川県",
-    "新潟県",
-    "富山県",
-    "石川県",
-    "福井県",
-    "山梨県",
-    "長野県",
-    "岐阜県",
-    "静岡県",
-    "愛知県",
-    "三重県",
-    "滋賀県",
-    "京都府",
-    "大阪府",
-    "兵庫県",
-    "奈良県",
-    "和歌山県",
-    "鳥取県",
-    "島根県",
-    "岡山県",
-    "広島県",
-    "山口県",
-    "徳島県",
-    "香川県",
-    "愛媛県",
-    "高知県",
-    "福岡県",
-    "佐賀県",
-    "長崎県",
-    "熊本県",
-    "大分県",
-    "宮崎県",
-    "鹿児島県",
-    "沖縄県"
-]
-# 県名を除いたリスト
-prefectures_without_name = [prefecture[:-1] for prefecture in prefectures]
-# 都道府県名 -> spot&intro
-
-
+#都道府県名 -> spot&intro
 @app.get("/pref/{pref_name}")
 async def read_pref(pref_name: str):
-    if pref_name in (prefectures) or pref_name in (prefectures_without_name):
-        pref = pref_introduction(pref=pref_name)
+    pref = pref_introduction(pref=pref_name)
+    if pref: 
         return pref
     else:
         raise HTTPException(status_code=404, detail="pref not found")
