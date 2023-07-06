3eb82231bfe5bfd3a91c8bc50d5ca694fc7ddf36
m213 <>
2023-07-06 09:59:26 +0900

    update frontend by wll 07060959

--- frontend/src/views/Result.jsx
+++ frontend/src/views/Result.jsx
@@ -34,68 +34,32 @@ import Search from "../components/Search";
 // index page sections
 
 const Result = () => {
+  //search keyword
   const contextValue = useContext(SearchStrContext);
   const searchStr = contextValue.searchStr;
   const searchKey = { pref_name: searchStr };
+
+  //search api route
   const request_search = "/combined";
   const request_weather = "/weather";
   const [content, setContent] = useState([]);
   const [carousel, setCarousel] = useState([]);
+  const [weather, setWeather] = useState();
   const [loading, setLoading] = useState(false);
   const [blank, setBlank] = useState(false);
-  const [weather, setWeather] = useState();
-
-  // const c1 = [
-  //   {
-  //     id: 1,
-  //     spot: "旭山動物園",
-  //     spot_en: "Asahiyama Zoo",
-  //     introduction:
-  //       "北海道にある動物園で、多くの動物たちを見ることができます。特に有名なのは、冬になると行われるペンギンのウォークです。",
-  //     pics: ["url1", "url2", "url3", "url4", "url5"],
-  //   },
-  //   {
-  //     id: 2,
-  //     spot: "函館山",
-  //     spot_en: "Mount Hakodate",
-  //     introduction:
-  //       "函館市にある山で、展望台からは函館市街や夜景を一望することができます。特に夜景は美しく、観光客に人気です。",
-  //     pics: ["url1", "url2", "url3", "url4", "url5"],
-  //   },
-  //   {
-  //     id: 3,
-  //     spot: "美瑛の丘",
-  //     spot_en: "Biei Hills",
-  //     introduction:
-  //       "美しい風景が広がる美瑛町にある丘です。四季折々の風景が楽しめ、特に夏には美しい花畑が広がります。写真撮影スポットとしても人気です。",
-  //     pics: ["url1", "url2", "url3", "url4", "url5"],
-  //   },
-  // ];
-
-  // const carousel = [
-  //   {
-  //     src: require("assets/img/theme/img-1-1200x1000.jpg"),
-  //     altText: "",
-  //     caption: "",
-  //     header: "",
-  //   },
-  //   {
-  //     src: require("assets/img/theme/img-2-1200x1000.jpg"),
-  //     altText: "",
-  //     caption: "",
-  //     header: "",
-  //   },
-  // ];
 
   useEffect(() => {
     console.log(searchKey);
+
+    //stop when loading stop
     if (loading) {
       return;
     }
+
     instance
       .post(request_search, searchKey)
       .then((response) => {
-        console.log(response.data);
+        console.log("text response:" + response.data);
         if (response.data === false) {
           setBlank(true);
           setLoading(true);
@@ -112,17 +76,24 @@ const Result = () => {
           if (content[2].pics.length > 0) {
             setCarousel((current) => [...current, { src: content[2].pics[0] }]);
           }
+          setBlank(false);
           setLoading(true);
         }
       })
       .catch((error) => console.log(error));
-    instance
-    .post(request_weather,searchKey)
-    .then((response)=>{
-      console.log(response.data);
-      setWeather(response.data);
-    })
-    .catch((error) => console.log(error));
+
+      instance
+      .post(request_weather, searchKey)
+      .then((response) => {
+        console.log("weather response:" + response.data);
+        if (response.data === false) {
+          setBlank(true);
+          setLoading(true);
+        }
+        setWeather(response.data);
+      })
+      .catch((error) => console.log(error));
+
   }, [content, searchKey]);
 
   return (
@@ -186,7 +157,7 @@ const Result = () => {
         ) : (
           <div className="main-content">
             <TrNavbar />
-            <Title items={carousel} place={searchStr} weather={weather}/>
+            <Title items={carousel} place={searchStr} weather={weather} />
 
             <Intro spot={content[0].spot} intro={content[0].introduction} />
             <Gallery pics={content[0].pics} />
